609cb62f0fc6d1f56aaad0f99f176f6db7b7df0f
Einas Madi <einas.madi@dyedurham.com>
2025-03-12 13:59:43 -0400

    Fix nextjs config to properly work with turbo

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,8 @@
-import withImages from 'next-images';
-
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['localhost'],
   },
 };
 
-export default withImages(nextConfig);
+export default nextConfig;
